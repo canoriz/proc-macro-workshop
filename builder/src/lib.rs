4c18be4dbeafeac520d03b4aec38e06306f05c76
canoriz <83388275+canoriz@users.noreply.github.com>
2024-01-07 21:27:21 +0800

    builder 03, 04

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,5 +1,4 @@
-use proc_macro2::TokenStream;
-use quote::{format_ident, quote, quote_spanned};
+use quote::{format_ident, quote};
 use syn::{self, parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
@@ -38,11 +37,72 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         }
     };
 
+    let builder_setter_funcs = match origin_fields {
+        syn::Fields::Named(ref fields) => {
+            let recurse = fields.named.iter().map(|f| {
+                let field_name = &f.ident;
+                let field_type = &f.ty;
+                quote! {
+                    fn #field_name(&mut self, #field_name: #field_type) -> &mut Self {
+                        self.#field_name = Some(#field_name);
+                        self
+                    }
+                }
+            });
+            quote! { #(#recurse)*}
+        }
+        _ => {
+            quote! {}
+        }
+    };
+
+    let check_builder_field_not_none = match origin_fields {
+        syn::Fields::Named(ref fields) => {
+            let recurse = fields.named.iter().map(|f| {
+                let field_name = &f.ident;
+                let field_name_string = quote!{
+                    #field_name
+                }.to_string();
+                quote! {
+                    if let None = self.#field_name {
+                        return Err(format!("{} not set", #field_name_string).into());
+                    }
+                }
+            });
+            quote! { #(#recurse)*}
+        }
+        _ => {
+            quote! {}
+        }
+    };
+
+    let builder_fields_filled = match origin_fields {
+        syn::Fields::Named(ref fields) => {
+            let recurse = fields.named.iter().map(|f| {
+                let field_name = &f.ident;
+                quote! {#field_name: self.#field_name.clone().unwrap()}
+            });
+            quote! { #(#recurse),*}
+        }
+        _ => {
+            quote! {}
+        }
+    };
+
     let tokens = proc_macro::TokenStream::from(quote! {
-        pub struct #builder_type {#builder_fields_def}
+        struct #builder_type {#builder_fields_def}
         impl #origin_name {
             pub fn builder() -> #builder_type {
                 #builder_type {#builder_fields_empty}
+            }
+        }
+        impl #builder_type {
+            #builder_setter_funcs
+            fn build(&mut self) -> Result<#origin_name, Box<dyn std::error::Error>> {
+                #check_builder_field_not_none
+                Ok(#origin_name{
+                    #builder_fields_filled
+                })
             }
         }
     });
@@ -56,22 +116,4 @@ fn builder_struct_fields(data: &syn::Data) -> syn::Fields {
     } else {
         syn::Fields::Unit
     }
-    // match *data {
-    //     syn::Data::Struct(ref data) => match data.fields {
-    //         syn::Fields::Named(ref fields) => {
-    //             let recurse = fields.named.iter().map(|f| {
-    //                 let name = &f.ident;
-    //                 let ty = &f.ty;
-    //                 quote!{#name: Option<#ty>}
-    //             });
-    //             quote!{{ #(#recurse),*}}
-    //         }
-    //         _ => {
-    //             quote! {}
-    //         }
-    //     },
-    //     _ => {
-    //         quote! {}
-    //     }
-    // }
 }
